009d3ba6c305c60bdada7da552c1310bbce5f40c
BuhariSahith <buharisahith@gmail.com>
2021-10-30 20:49:51 +0530

    assets added

---
+++ assets/libs/index.js
@@ -0,0 +1,14 @@
+
+
+// <!-- typed js effect starts -->
+let typed = new Typed(".typing-text", {
+    strings: ["Software developer ...!" ],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 25,
+    backDelay: 500,
+    
+  });
+// <!-- typed js effect ends -->
+
+
