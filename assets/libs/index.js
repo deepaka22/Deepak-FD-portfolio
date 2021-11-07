e96f81ae5ec4fd634b2cbd128607f91d34d0505f
BuhariSahith <buharisahith@gmail.com>
2021-11-07 13:20:31 +0530

    pre-loadings implemented

--- assets/libs/index.js
+++ assets/libs/index.js
@@ -1,14 +1,21 @@
-
-
 // <!-- typed js effect starts -->
 let typed = new Typed(".typing-text", {
-    strings: ["Software developer ...!" ],
-    loop: true,
-    typeSpeed: 80,
-    backSpeed: 25,
-    backDelay: 500,
-    
-  });
+  strings: ["Software developer ...!"],
+  loop: true,
+  typeSpeed: 80,
+  backSpeed: 25,
+  backDelay: 500,
+});
 // <!-- typed js effect ends -->
 
+// pre loader start
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+function fadeOut() {
+  setInterval(loader, 1000);
+}
+
+window.onload = fadeOut; // Browser will call
+// pre loader end
 
