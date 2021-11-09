aef3ed3f028b3e0f7e6f05071d562496814a2b45
BuhariSahith <buharisahith@gmail.com>
2021-11-09 16:46:13 +0530

    added animation while scrolling the page

--- assets/libs/index.js
+++ assets/libs/index.js
@@ -1,4 +1,4 @@
-// <!-- typed js effect starts -->
+// Typed Effect starts 
 let typed = new Typed(".typing-text", {
   strings: ["Software developer ...!"],
   loop: true,
@@ -6,9 +6,9 @@ let typed = new Typed(".typing-text", {
   backSpeed: 25,
   backDelay: 500,
 });
-// <!-- typed js effect ends -->
 
-// pre loader start
+
+// Pre Loading 
 function loader() {
   document.querySelector(".loader-container").classList.add("fade-out");
 }
@@ -17,5 +17,62 @@ function fadeOut() {
 }
 
 window.onload = fadeOut; // Browser will call
-// pre loader end
+
+
+
+// Scroll Animations
+const scrlAnime = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 1000,
+  reset: true
+});
+
+/* SCROLL HOME */
+scrlAnime.reveal('.home .content h3',{delay: 200}); 
+scrlAnime.reveal('.home .content p',{delay: 200}); 
+scrlAnime.reveal('.home .content .btn',{delay: 200}); 
+
+scrlAnime.reveal('.home .content .socials',{interval: 600}); 
+scrlAnime.reveal('.home .content .socials .social-icons .linkedin',{delay: 300}); 
+scrlAnime.reveal('.home .content .socials .social-icons .github',{delay: 450});
+scrlAnime.reveal('.home .content .socials .social-icons .twitter',{delay: 550}); 
+scrlAnime.reveal('.home .content .socials .social-icons .instagram',{delay: 650}); 
+scrlAnime.reveal('.home .content .socials .social-icons .facebook',{delay: 750}); 
+
+scrlAnime.reveal('.home .image',{delay: 750}); 
+
+/* SCROLL SKILLS */
+scrlAnime.reveal('.skills .content-skills .heading',{delay: 400});
+scrlAnime.reveal('.skills .content-skills p',{interval: 600}); 
+scrlAnime.reveal('.skills .tech-skills',{delay: 400}); 
+scrlAnime.reveal('.skills .tech-skills .tech-icon',{interval: 200}); 
+
+
+/* SCROLL EXPERIENCE */
+scrlAnime.reveal('.experience .heading',{delay: 400});
+scrlAnime.reveal('.experience .timeline',{delay: 400});
+scrlAnime.reveal('.experience .timeline .right',{interval: 400}); 
+
+scrlAnime.reveal('.experience .exp-sec-wrapper .image',{delay: 500});
+
+/* SCROLL PROJECTS */
+scrlAnime.reveal('.projects .heading',{delay: 400}); 
+scrlAnime.reveal('.projects .projects-card-wrapper',{interval: 600}); 
+scrlAnime.reveal('.projects .projects-card-wrapper .pr-card-one',{delay: 400}); 
+scrlAnime.reveal('.projects .projects-card-wrapper .pr-card-two',{delay: 600}); 
+scrlAnime.reveal('.projects .projects-card-wrapper .pr-card-three',{delay: 800}); 
+
+
+
+
+
+/* SCROLL EDUCATION */
+scrlAnime.reveal('.education .heading',{delay: 400}); 
+scrlAnime.reveal('.education .edu-center',{delay: 400}); 
+scrlAnime.reveal('.education .edu-center .edu-details',{interval: 500}); 
+
+
+/* SCROLL FOOTER */
+scrlAnime.reveal('.footer ',{delay: 400});
 
