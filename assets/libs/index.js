acb5c8c7e003d00e13d647311615c61641b9251d
BuhariSahith <buharisahith@gmail.com>
2021-11-29 17:22:17 +0530

    integrating the live - chat

--- assets/libs/index.js
+++ assets/libs/index.js
@@ -28,6 +28,20 @@ function scroller() {
   });
 }
 
+// Start of Tawk.to Script
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61a4b3e99099530957f71d53/1fllltit5';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+// End of Tawk.to Script
+
 // Typed Effect starts
 let typed = new Typed(".typing-text", {
   strings: ["Software developer .......!"],
